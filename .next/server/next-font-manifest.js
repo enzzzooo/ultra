a61e80af70ccd59fb7bd332361214facadaf7572
enzzzooo <ed123658@gmail.com>
2023-12-23 09:03:05 -0600

    git ignore

--- .next/server/next-font-manifest.js
+++ .next/server/next-font-manifest.js
@@ -1,1 +1,1 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/stef/Documents/ultra-starter/app/layout\":[\"static/media/be78684d432e3822-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{},\"appUsingSizeAdjust\":false,\"pagesUsingSizeAdjust\":false}"
